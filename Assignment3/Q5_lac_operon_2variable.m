920b50e9ea9a30a86c928688e387230c80ea7a69
Gene Cho <genec2008@gmail.com>
2014-05-17 22:20:31 -0700

    Closer to finishing Q5

--- Assignment3/Q5_lac_operon_2variable.m
+++ Assignment3/Q5_lac_operon_2variable.m
@@ -7,23 +7,48 @@
 % lac operon. The differential equations governing this system are as
 % follows:
 
+% constants
 beta = 1 ;
 gamma = 1 ;
-rho = 0.2
-l0 = 4 ;
+delta = 0.2
+lzero = 4 ;
 p = 4 ;
-s = 1 ;
-lext = 2.5 ; % external lactose
+sigma = 1 ;
 
-% The variable (lext), representing extracellular lactose, is the variable that
-% can vary and potentially cause the bacteria to change states. Plot the
-% nullclines of this model for lext=2.5. Because of the l^4 term appearing in
-% the second equation, it is easiest to treat LacY as the dependent
-% variable (ordinate) and l as the independent variable (abscissa). Which
-% of the following statements is correct?
+% l = intracellular lactose (independent variable)
+l = 0:0.05:15 ;
+
+% lext (external lactose) is the variable that can vary and potentially
+% cause the bacteria to change states.
+% Plot the nullclines of this model for lext=2.5.
+
+lext = 2.5 ; 
+
+% Because of the l^4 term appearing in the second equation, it is easiest
+% to treat LacY as the dependent variable (ordinate) and l as the
+% independent variable (abscissa).
+
+% Nucline for lactose (dl/dt)
+% Solve dl/dt=0 
+% --> FR: LacY =(gamma*l)/(beta*lext) ;
+%
+% Nullcline for LacY (dLacY/dt)
+% Solve dLacY/dt=0 
+% --> BR: LacY =(delta + p*(l^4/(l^4 + l0^4)))/sigma
+
+% Which of the following statements is correct?
+%
+% A. Nullclines intersect in three points which all of them are stable
+% steady states.
+% B. Above the LacY nullcline, LacY and  are increasing. 
+% C. Nullclines intersect in three points which one of them is an unstable
+% steady states.
+% D. Nullclines intersect in two points which both of them are unstable
+% steady states.
 
 
-% Plot Forward/Backward Rates vs Phosphorylation
+
+% Plot LacY vs l (intracellular lactose)
 figure
 handle1 = gcf ;  % "get current figure"
 hold on 
@@ -36,12 +61,12 @@ handle2 = gcf ;
 hold on
 
 % iterate for varying increments of Kmb
-for i=1:length(Kmb)
-  BR = kminus*(Astar./(Astar + Kmb(i))) ;  
-  FR = (kfs.*Astar).*(1-Astar) ;
+for i=1:length(lext)
+  BR = (gamma.*l)/(beta*lext) ;
+  FR = (delta + (p .* ((l.^4) ./ ((l.^4) + (lzero^4)))))./sigma
   figure(handle1)
-  plot(Astar,FR,'b','LineWidth',2)
-  plot(Astar,BR,'r','LineWidth',2)
+  plot(l,FR,'b','LineWidth',2)
+  plot(l,BR,'r','LineWidth',2)
   
   crossings = [] ;
   difference = FR-BR ;
@@ -51,16 +76,17 @@ for i=1:length(Kmb)
     end
   end
   figure(handle2)
-  plot(Kmb(i),Astar(crossings),'bo')
+  plot(lext(i),l(crossings),'bo')
   
 end
 
 % decorate plots
 figure(handle1)
-axis([0 1 0 max(FR)])
+axis([0 max(l) 0 max(FR)])
 set(gca,'TickDir','Out')
-xlabel('[A*]/[A]')
-ylabel('Rates')
+xlabel('[LacY]')
+ylabel('[lactose]')
+title('Lac Operon Model for E. Coli')
 
 figure(handle2)
 set(gca,'TickDir','Out')
