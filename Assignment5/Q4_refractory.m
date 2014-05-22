ef8fc7f869be9c34fdd8bbaf42d34964ca5c901b
troutbum <genec2008@gmail.com>
2014-05-21 17:45:02 -0700

    Completed all assignments
    Finish Assignment 5 Q4 refractory plots

--- Assignment5/Q4_refractory.m
+++ Assignment5/Q4_refractory.m
@@ -113,102 +113,126 @@ stimdelay = 1 ;
 stimdur = 1 ;
 stim_amp = -10 ;
 
-stim_start = stimdelay ;
-stim_end = stimdelay + stimdur ;
-
 % % % Intervals defined as follows
 % % % 1) t=0 zero to beginning of stimulus
 % % % 2) beginning to end of stimulus
-% % % 3) end of stimulus to end of simulation
-simints = 3 ;
+% % % 3) refractory period
+% % % 4) beginning to end of 2nd stimulus
+% % % 3) end of 2nd stimulus to end of simulation
 
-intervals(1,:) = [0,stim_start] ;
-intervals(2,:) = [stim_start,stim_end] ;
-intervals(3,:) = [stim_end,tend] ;
+simints = 5 ;           % increase intervals to 5 for 2nd spike 
 
-Istim(1) = 0 ;
+Istim(1) = 0 ;          % define stimuli amplitudes
 Istim(2) = stim_amp ;
 Istim(3) = 0 ;
+Istim(4) = stim_amp ;   % add for 2nd spike
+Istim(5) = 0 ;          % add for 2nd spike
+
+refracs = [6:21] ;      % set of refractory time intervals to test
+
+figure                  % plot all results in a single window
+for ii=1:length(refracs)
+    
+    stim_start = stimdelay ;
+    stim_end = stimdelay + stimdur ;
+    stim2_start = stim_end + refracs(ii) ;
+    stim2_end = stim2_start + stimdur ;
+
+    intervals(1,:) = [0,stim_start] ;
+    intervals(2,:) = [stim_start,stim_end] ;
+    intervals(3,:) = [stim_end,stim2_start] ;  % redefine and add intervals
+    intervals(4,:) = [stim2_start,stim2_end] ; % for 2nd stimulus
+    intervals(5,:) = [stim2_end,tend] ;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% Step 3:  Set initial conditions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-V = -60 ;
-m = 0 ;
-h = 0.6 ;
-n = 0.3 ;
-
+    V = -60 ;
+    m = 0 ;
+    h = 0.6 ;
+    n = 0.3 ;
+    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% Step 4:  Loop through and solve model
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-statevar_i = [V,m,h,n] ;
+    statevar_i = [V,m,h,n] ;
 
 % % Simulate 60 seconds at rest before stimulus applied
-[post,posstatevars] = ode15s(@dydt_hh,[0,60000],statevar_i,[],0) ;
-statevar_i = posstatevars(end,:) ;
+    [post,posstatevars] = ode15s(@dydt_hh,[0,60000],statevar_i,[],0) ;
+    statevar_i = posstatevars(end,:) ;
 
-t = 0 ;
-statevars = statevar_i ;
-for i=1:simints
-  [post,posstatevars] = ode15s(@dydt_hh,intervals(i,:),statevar_i,[],Istim(i)) ;
-  t = [t;post(2:end)] ;
-  statevars = [statevars;posstatevars(2:end,:)] ;
-  statevar_i = posstatevars(end,:) ;
+    t = 0 ;
+    statevars = statevar_i ;
+    for i=1:simints
+        [post,posstatevars] = ode15s(@dydt_hh,intervals(i,:),statevar_i,[],Istim(i)) ;
+        t = [t;post(2:end)] ;
+        statevars = [statevars;posstatevars(2:end,:)] ;
+        statevar_i = posstatevars(end,:) ;
+    end
+
+    outputcell = num2cell(statevars,1) ;
+
+    [V,m,h,n] = deal(outputcell{:}) ;
+
+    gNa = GNa*m.^3.*h;
+    INa = gNa.*(V-ENa);
+
+    gK = GK*n.^4; 
+    IK = gK.*(V-EK);
+
+    Il = Gl*(V-El) ;
+    Iion = INa + IK + Il ;
+
+    colors = repmat('krgmbc',1,500) ;
+    hold on
+    plot(t,V,colors(ii))
+    title('Refractory Intervals')
+    figurelegend{ii} = [num2str(refracs(ii)), ' ms']; 
+    legend(figurelegend,'Location','Southeast')
+    xlabel('time (ms)')
+    ylabel('V_m (mV)')
 end
-
-outputcell = num2cell(statevars,1) ;
-
-[V,m,h,n] = deal(outputcell{:}) ;
-
-gNa = GNa*m.^3.*h;
-INa = gNa.*(V-ENa);
-
-gK = GK*n.^4; 
-IK = gK.*(V-EK);
-
-Il = Gl*(V-El) ;
-Iion = INa + IK + Il ;
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% Step 5:  Plot or write output to files
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-figure
-subplot(2,2,1)
-plot(t,V)
-set(gca,'TickDir','Out')
-xlabel('time (ms)')
-ylabel('V_m (mV)')
 
-subplot(2,2,2)
-hold on
-plot(t,INa,'b')
-plot(t,IK,'r')
-plot(t,Il,'g')
-set(gca,'TickDir','Out')
-legend('I_N_a','I_K','I_L')
-xlabel('time (ms)')
-ylabel('Ionic current')
-hold off
-
-subplot(2,2,3)
-hold on
-plot(t,gNa,'b')
-plot(t,gK,'r')
-set(gca,'TickDir','Out')
-legend('g_N_a','g_K')
-xlabel('time (ms)')
-ylabel('Conductances')
-
-subplot(2,2,4)
-hold on
-plot(t,m,'b')
-plot(t,h,'r')
-plot(t,n,'g')
-set(gca,'TickDir','Out')
-legend('m','h','n')
-xlabel('time (ms)')
-ylabel('Gating variables')
+% figure
+% subplot(2,2,1)
+% plot(t,V)
+% set(gca,'TickDir','Out')
+% xlabel('time (ms)')
+% ylabel('V_m (mV)')
+% 
+% subplot(2,2,2)
+% hold on
+% plot(t,INa,'b')
+% plot(t,IK,'r')
+% plot(t,Il,'g')
+% set(gca,'TickDir','Out')
+% legend('I_N_a','I_K','I_L')
+% xlabel('time (ms)')
+% ylabel('Ionic current')
+% hold off
+% 
+% subplot(2,2,3)
+% hold on
+% plot(t,gNa,'b')
+% plot(t,gK,'r')
+% set(gca,'TickDir','Out')
+% legend('g_N_a','g_K')
+% xlabel('time (ms)')
+% ylabel('Conductances')
+% 
+% subplot(2,2,4)
+% hold on
+% plot(t,m,'b')
+% plot(t,h,'r')
+% plot(t,n,'g')
+% set(gca,'TickDir','Out')
+% legend('m','h','n')
+% xlabel('time (ms)')
+% ylabel('Gating variables')
 
 
